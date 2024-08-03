79fd15fe5cd9f573d2c419740f24f2af7e596860
sathish257216 <sathish.257216@gmail.com>
2024-08-03 01:33:13 -0400

    load data using fetxh

--- foodtech/src/Home/Home.js
+++ foodtech/src/Home/Home.js
@@ -1,45 +1,78 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import ResturantCardComponent from "../shared/components/ResturantCard";
-import { hotelDetails } from "../utils/mockData";
-
 
 const HomeComponent = () => {
-    const [listOfResturants, setListOfResturants] = useState(hotelDetails);
+  let resturantDetails = [];
+  const [listOfResturants, setListOfResturants] = useState();
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+    fetchResturantData();
+  }, []);
 
-    
-    function filterResturant() {
-        const filterList = listOfResturants.filter(resturant => resturant.info.avgRating > 4);
-        console.log('filterList', filterList);
-        setListOfResturants(filterList);
-    }
-    
-    function searchResturant() {
-        console.log('Search resturant');
-    }
+  const fetchResturantData = async () => {
+    setIsLoading(true);
+    fetch("/resturant.json")
+      .then((response) => response.json())
+      .then((data) => {
+        resturantDetails = data.resturant;
+        setListOfResturants(resturantDetails);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        setIsLoading(false);
+        console.error("Error fetching the data:", error);
+      });
+  };
 
-    function resetResturant() {
-        setListOfResturants(hotelDetails);
-    }
+  function filterResturant() {
+    const filterList = listOfResturants.filter(
+      (resturant) => resturant.info.avgRating > 4
+    );
+    console.log("filterList", filterList);
+    setListOfResturants(filterList);
+  }
 
-    return (
-      <div className="res-container"> 
-        <div className="top-bar">
-            <div className="filter-bar">
-                <button className="filter-btn common-btn" onClick={filterResturant}>Top Rated</button>  
-                <button className="filter-btn common-btn" onClick={resetResturant}>Reset</button>  
-            </div>
-            <div className="serach-bar">
-                <input type="text" className="search-input" placeholder="Search..." ></input>
-                <button className="search-btn common-btn" onClick={searchResturant}>Search</button>
-            </div>
+  function searchResturant() {
+    console.log("Search resturant");
+  }
+
+  function resetResturant() {
+    setListOfResturants(resturantDetails);
+  }
+
+  return (
+    <div className="res-container">
+      <div className="top-bar">
+        <div className="filter-bar">
+          <button className="filter-btn common-btn" onClick={filterResturant}>
+            Top Rated
+          </button>
+          <button className="filter-btn common-btn" onClick={resetResturant}>
+            Reset
+          </button>
         </div>
-        <div className="card-container">
-          {
-            listOfResturants?.map(hotel => <ResturantCardComponent key={hotel.info.id} resData={hotel} />)
-          }
+        <div className="serach-bar">
+          <input
+            type="text"
+            className="search-input"
+            placeholder="Search..."
+          ></input>
+          <button className="search-btn common-btn" onClick={searchResturant}>
+            Search
+          </button>
         </div>
       </div>
-    )
-}
+      {isLoading ? (
+        <div>Loading...</div>
+      ) : (
+        <div className="card-container">
+          {listOfResturants?.map((hotel) => (
+            <ResturantCardComponent key={hotel.info.id} resData={hotel} />
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
 
 export default HomeComponent;
--- foodtech/src/utils/constant.js
+++ foodtech/src/utils/constant.js
@@ -1,1 +1,2 @@
-export const CDN_URL= "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/"
+export const CDN_URL= "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
+export const RESTURANT_API_URL = "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9352403&lng=77.624532&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
