6da42d780ba2a6f01d83c72d5131eb4a279f8194
sathish257216 <sathish.257216@gmail.com>
2024-08-02 23:42:09 -0400

    update state

--- foodtech/src/Home/Home.js
+++ foodtech/src/Home/Home.js
@@ -1,20 +1,41 @@
+import { useState } from "react";
 import ResturantCardComponent from "../shared/components/ResturantCard";
 import { hotelDetails } from "../utils/mockData";
 
-function searchResturant() {
-    console.log('Search resturant');
-}
 
 const HomeComponent = () => {
+    const [listOfResturants, setListOfResturants] = useState(hotelDetails);
+
+    
+    function filterResturant() {
+        const filterList = listOfResturants.filter(resturant => resturant.info.avgRating > 4);
+        console.log('filterList', filterList);
+        setListOfResturants(filterList);
+    }
+    
+    function searchResturant() {
+        console.log('Search resturant');
+    }
+
+    function resetResturant() {
+        setListOfResturants(hotelDetails);
+    }
+
     return (
       <div className="res-container"> 
-        <div className="serach-bar">
-          <input type="text" className="search-input" placeholder="Search..." ></input>
-          <button className="search-btn" onClick={searchResturant}>Search</button>
+        <div className="top-bar">
+            <div className="filter-bar">
+                <button className="filter-btn common-btn" onClick={filterResturant}>Top Rated</button>  
+                <button className="filter-btn common-btn" onClick={resetResturant}>Reset</button>  
+            </div>
+            <div className="serach-bar">
+                <input type="text" className="search-input" placeholder="Search..." ></input>
+                <button className="search-btn common-btn" onClick={searchResturant}>Search</button>
+            </div>
         </div>
         <div className="card-container">
           {
-            hotelDetails.map(hotel => <ResturantCardComponent key={hotel.info.id} resData={hotel} />)
+            listOfResturants?.map(hotel => <ResturantCardComponent key={hotel.info.id} resData={hotel} />)
           }
         </div>
       </div>
